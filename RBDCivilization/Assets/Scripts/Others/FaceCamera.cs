7ccc90dc43c70352c39f831caff641e131330568
AXBNBS <44948868+AXBNBS@users.noreply.github.com>
2019-12-16 15:14:53 +0100

    Espasetes
    Jajajajaj me vuic morir

---
+++ RBDCivilization/Assets/Scripts/Others/FaceCamera.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform camera;
+
+
+    // Start is called before the first frame update.
+    private void Start ()
+    {
+        camera = Camera.main.transform;
+    }
+
+
+    // Update is called once per frame.
+    private void Update ()
+    {
+        this.transform.rotation = Quaternion.LookRotation (camera.position - this.transform.position);
+    }
+}
